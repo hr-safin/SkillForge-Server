5e93d9f52b496f1426c3f9d254405de2871d8336
safinhr <hrsafin2434@gmail.com>
2024-01-12 22:39:04 +0600

    get enroll course

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const cors =  require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port  = process.env.PORT || 5000
 
 // middleware
@@ -33,6 +33,13 @@ async function run() {
         const cursor = courseCollection.find()
         const result = await cursor.toArray()
         res.send(result)
+    })
+
+    app.get("/enroll/:id", async(req, res) => {
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await courseCollection.findOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
