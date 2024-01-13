88da922c150da106d2ef4c1eb2f4ec9dfad66d37
safinhr <hrsafin2434@gmail.com>
2024-01-13 11:12:56 +0600

    removed credential

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+require("dotenv").config();
 const cors =  require("cors")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port  = process.env.PORT || 5000
@@ -11,7 +12,7 @@ app.use(cors())
 //USX749MhNgJHA9S5
 
 
-const uri = "mongodb+srv://rahmansafin559:USX749MhNgJHA9S5@cluster0.xat17xd.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xat17xd.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
