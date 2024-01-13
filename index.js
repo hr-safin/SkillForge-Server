8f6fd77d4f5bfc906654935494a3dab22f590299
safinhr <hrsafin2434@gmail.com>
2024-01-13 23:45:23 +0600

    updated course enroll path

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
         res.send(result)
     })
 
-    app.get("/course/enroll/:id", async(req, res) => {
+    app.get("/enroll/:id", async(req, res) => {
       const id = req.params.id
       const query = {_id : new ObjectId(id)}
       const result = await courseCollection.findOne(query)
