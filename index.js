40c104b80ebe419870d8ea0ec48f6e3800ec5ee0
safinhr <hrsafin2434@gmail.com>
2024-01-15 20:58:23 +0600

    post paymentIntent and setup SK

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 require("dotenv").config();
 const cors =  require("cors")
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port  = process.env.PORT || 5000
 
@@ -48,6 +49,26 @@ async function run() {
       const query = {_id : new ObjectId(id)}
       const result = await courseCollection.findOne(query)
       res.send(result)
+    })
+
+
+    // payment intent
+
+    app.post("/create-payment-intent", async(req, res) => {
+      const {price} = req.body
+      const amount = parseInt(price * 100) 
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency : "usd",
+        payment_method_types : [
+          "card"
+        ]
+      })
+
+      res.send({
+        clientSecret : paymentIntent.client_secret
+      })
     })
 
     // Send a ping to confirm a successful connection
