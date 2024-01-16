209db4f74807b5f781f7dee388be512e32676020
safinhr <hrsafin2434@gmail.com>
2024-01-16 15:52:48 +0600

    implement jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 require("dotenv").config();
 const cors =  require("cors")
+const jwt = require("jsonwebtoken")
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port  = process.env.PORT || 5000
